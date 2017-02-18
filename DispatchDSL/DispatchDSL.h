1b91aee223569264f71711ce93f4b8ab52eab275
Shayan <sh.taslim@gmail.com>
2017-02-18 13:16:29 -0500

    Initial Commit

---
+++ DispatchDSL/DispatchDSL.h
@@ -0,0 +1,19 @@
+//
+//  DispatchDSL.h
+//  DispatchDSL
+//
+//  Created by Shayan on 2/18/17.
+//  Copyright © 2017 Shayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DispatchDSL.
+FOUNDATION_EXPORT double DispatchDSLVersionNumber;
+
+//! Project version string for DispatchDSL.
+FOUNDATION_EXPORT const unsigned char DispatchDSLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DispatchDSL/PublicHeader.h>
+
+
